232e382ac3f29c9288c428c23d58d7d05f4c2782
JamalNiga <141185853+JamalNiga@users.noreply.github.com>
2023-09-24 13:48:43 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,6 +1,8 @@
 sudo apt update -y
-sudo apt install python3 aircrack-ng -y
+sudo apt install python3 aircrack-ng wget -y
 git clone https://github.com/JamalNiga/capbrute
 cd capbrute
 python3 hy.py
+rm mp.cap
+wget https://github.com/JamalNiga/capbrute/raw/main/mp.cap
 sudo aircrack-ng -w pass.txt mp.cap
