d9990556db560a5f1b3c552bae96dee197a68647
JamalNiga <141185853+JamalNiga@users.noreply.github.com>
2023-10-07 01:30:43 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,6 +1,6 @@
 sudo apt update -y
 sudo apt install python3 aircrack-ng hashcat wget -y
-git clone https://github.com/JamalNiga/capbrute.git
+git clone https://github.com/JamalNiga/capbrute
 cd capbrute
 #python3 hy.py
 #sudo aircrack-ng -w pass.txt handshake_TPLINK959142_18-A6-F7-95-91-42_2023-10-05T20-46-49.cap
