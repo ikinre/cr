477ecf41f8d053a0dd7321e7c3dd21587d91581f
JamalNiga <141185853+JamalNiga@users.noreply.github.com>
2023-10-07 01:29:41 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,8 +1,8 @@
 sudo apt update -y
 sudo apt install python3 aircrack-ng hashcat wget -y
-git clone https://github.com/JamalNiga/capbrute
+git clone https://github.com/JamalNiga/capbrute.git
 cd capbrute
 #python3 hy.py
 #sudo aircrack-ng -w pass.txt handshake_TPLINK959142_18-A6-F7-95-91-42_2023-10-05T20-46-49.cap
 #-.
-sudo haschcat –m 2200 –a 3 tplink.hc22000 ?l ?l ?l ?l ?l ?l ?l ?l ?l ?l
+sudo hashcat –m 2200 –a 3 tplink.hc22000 ?l ?l ?l ?l ?l ?l ?l ?l ?l ?l
