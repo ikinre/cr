9f17bf5a502af52e82d3cb014695c010ed534026
JamalNiga <141185853+JamalNiga@users.noreply.github.com>
2023-10-07 10:29:11 +0100

    Update bwi.sh

--- b.sh
+++ b.sh
@@ -2,4 +2,4 @@ sudo apt-get update-y
 sudo apt install aircrack-ng hashcat -y
 git clone https://github.com/JamalNiga/capbrute.git cap
 cd cap
-sudo hashcat –m 22000 tplink.hc22000 -a 3 ?l ?l ?l ?l ?l ?l ?l ?l ?l ?l
+hashcat –m 22000 tplink.txt -a 3 ?l?l?l?l?l?l?l?l
