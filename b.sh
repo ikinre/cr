4e72bdd2c8e6ad4a2be8d880169b5e66082ed15e
JamalNiga <141185853+JamalNiga@users.noreply.github.com>
2023-10-07 01:38:48 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -2,4 +2,5 @@ sudo apt update -y
 sudo apt install python3 aircrack-ng hashcat wget -y
 git clone https://github.com/JamalNiga/capbrute.git cap
 cd cap
-sudo hashcat –m 2200 –a 3 tplink.hc22000 ?l ?l ?l ?l ?l ?l ?l ?l ?l ?l
+ls
+sudo hashcat –m 22000 –a 3 tplink.hc22000 ?l ?l ?l ?l ?l ?l ?l ?l ?l ?l
