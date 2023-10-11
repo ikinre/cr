7d9614acdd7b9099128707d2b2532b5d31f8587a
JamalNiga <141185853+JamalNiga@users.noreply.github.com>
2023-10-11 21:19:20 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -2,5 +2,6 @@ sudo apt-get update-y
 sudo apt install aircrack-ng python3 hashcat -y
 git clone https://github.com/JamalNiga/capbrute.git cap
 cd cap
+ls
 python3 hy.py
-aircrack-ng -w *txt handshake-88108F179892.cap
+aircrack-ng -w *txt 9861.cap
