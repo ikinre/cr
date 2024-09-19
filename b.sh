3e1cccda23da32fa0c37147c884374227904f15b
ikinre <145940251+ikinre@users.noreply.github.com>
2024-09-20 00:08:28 +0100

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,8 @@
+sudo apt update -y
+sudo apt install aircrack-ng python3 hashcat nano -y
+cd /root
+git clone https://github.com/ikinre/capbrute.git cap
+cd cap
+wget -O wordlist.txt https://github.com/kkrypt0nn/wordlists/raw/main/wordlists/languages/english.txt
+python3 hy.py
+aircrack-ng -w wordlist.txt handshake.cap
