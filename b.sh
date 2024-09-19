30e331f90b0b981b598fc26ca09acac2b46a8bb2
ikinre <145940251+ikinre@users.noreply.github.com>
2024-09-20 00:12:37 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,6 +3,6 @@ sudo apt install aircrack-ng python3 hashcat nano -y
 cd /root
 git clone https://github.com/ikinre/capbrute.git cap
 cd cap
-wget -O wordlist.txt https://github.com/kkrypt0nn/wordlists/raw/main/wordlists/languages/english.txt
+#wget -O wordlist.txt https://github.com/kkrypt0nn/wordlists/raw/main/wordlists/languages/english.txt
 python3 hy.py
 aircrack-ng -w wordlist.txt handshake.cap
