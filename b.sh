5a54807b681db13ab827bc3fdfac28afcf02787b
ikinre <145940251+ikinre@users.noreply.github.com>
2024-09-20 10:20:38 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,6 +3,6 @@ sudo apt install aircrack-ng python3 hashcat nano -y
 cd /root
 git clone https://github.com/ikinre/capbrute.git cap
 cd cap
-wget https://github.com/kkrypt0nn/wordlists/raw/main/wordlists/languages/english.txt -O wordlist.txt 
+wget https://github.com/danielmiessler/SecLists/blob/master/Passwords/2020-200_most_used_passwords.txt -O /2020-200_most_used_passwords.txt 
 python3 hy.py
-aircrack-ng -w wordlist.txt handshake.cap
+aircrack-ng -w /2020-200_most_used_passwords.txt handshake.cap
