0b9d184df942b13a762ba7956286f39606360fbb
ikinre <145940251+ikinre@users.noreply.github.com>
2024-09-20 12:14:17 +0100

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,6 +3,6 @@ sudo apt install aircrack-ng python3 hashcat nano -y
 cd /root
 git clone https://github.com/ikinre/capbrute.git cap
 cd cap
-wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt -O rockyou.txt 
+//wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt -O rockyou.txt 
 python3 hy.py
 aircrack-ng -w rockyou.txt handshake.cap
